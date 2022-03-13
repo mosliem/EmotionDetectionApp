8de9b69e99009c36a9758a6d687998747214e5fd
Mohamed Sliem <mohamedmostafa191299@gmail.com>
2022-03-13 02:37:45 +0200

    Solving copiling error

--- EmotionDetectionApp/Controllers/Dashboard/DashboardVC.swift
+++ EmotionDetectionApp/Controllers/Dashboard/DashboardVC.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import Charts
+class DashboardVC: UIViewController,ChartViewDelegate {
 
-class DashboardVC: UIViewController {
-
+    @IBOutlet weak var ProfileImageView: UIImageView!
+    @IBOutlet weak var NameLabel: UILabel!
+    @IBOutlet weak var currentMoodImageView: UIImageView!
+    @IBOutlet weak var currentMoodLabel: UILabel!
+    @IBOutlet weak var CurrentMoodView: UIView!
+    @IBOutlet weak var profileView: UIView!
+    
+    var chart = LineChartView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureProfileImageView()
+        configureCurrentMoodView()
+        configureMoodGraph()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureProfileImageView(){
+        
+        ProfileImageView.clipsToBounds = true
+        ProfileImageView.layer.cornerRadius = 40
+        
+        profileView.layer.cornerRadius = 40
+        profileView.layer.shadowColor = UIColor.darkGray.cgColor
+        profileView.layer.shadowOffset = CGSize.zero
+        profileView.layer.shadowRadius = 2
+        profileView.layer.shadowOpacity = 1.0
     }
-    */
-
+    private func configureCurrentMoodView(){
+        CurrentMoodView.layer.cornerRadius = 10
+        CurrentMoodView.layer.shadowColor = UIColor.darkGray.cgColor
+        CurrentMoodView.layer.shadowOffset = CGSize.zero
+        CurrentMoodView.layer.shadowRadius = 2
+        CurrentMoodView.layer.shadowOpacity = 0.9
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        chart = LineChartView(frame: CGRect(x: 0, y:CurrentMoodView.bottom + 50, width: view.width, height: 300))
+        view.addSubview(chart)
+        var entry = [ChartDataEntry]()
+        
+        for x in 1 ..< 10 {
+            entry.append(ChartDataEntry(x: Double(x), y: Double(x)))
+        }
+        let set = LineChartDataSet(entries: entry)
+        set.colors = ChartColorTemplates.material()
+        let data = LineChartData(dataSet: set)
+        chart.data = data
+        
+        
+    }
+    private func configureMoodGraph(){
+     
+    }
 }
--- EmotionDetectionApp/Resources/Extenstions.swift
+++ EmotionDetectionApp/Resources/Extenstions.swift
@@ -5,4 +5,37 @@
 //  Created by mohamedSliem on 3/13/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    func applyshadow(shadowRadius : CGFloat , shadowOpacity :Float , shadowColor : UIColor){
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = CGSize.zero
+        self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowRadius = shadowRadius
+        self.layer.shadowPath = UIBezierPath(rect:self.bounds).cgPath
+        self.clipsToBounds = false
+        self.layer.masksToBounds = false
+    }
+}
+extension UIView
+{
+    var height : CGFloat {
+        return frame.size.height
+    }
+    var width : CGFloat  {
+        return frame.size.width
+    }
+    var left : CGFloat {
+        return frame.origin.x
+    }
+    var right : CGFloat {
+        return left + width
+    }
+    var top : CGFloat {
+        return frame.origin.y
+    }
+    var bottom : CGFloat{
+        return top + height
+    }
+}
